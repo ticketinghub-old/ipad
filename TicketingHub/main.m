a3afb350a117fca4c4db20a789e561f8b43e6dc5
Mark Brindle <mark_brindle@me.com>
2013-06-06 12:47:34 +0100

    Initial Commit

---
+++ TicketingHub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TicketingHub
+//
+//  Created by Mark Brindle on 06/06/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TXHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([TXHAppDelegate class]));
+  }
+}
---
+++ TicketingHubTests/TicketingHubTests.h
@@ -0,0 +1,13 @@
+//
+//  TicketingHubTests.h
+//  TicketingHubTests
+//
+//  Created by Mark Brindle on 06/06/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TicketingHubTests : SenTestCase
+
+@end
