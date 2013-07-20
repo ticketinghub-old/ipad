db7375545d513c0bc9d2eda2af7d492c2fcf8eef
Mark Brindle <mark.brindle@arkemm.com>
2013-07-20 20:51:47 +0100

    Remove fake menu container & set additional buttons on main screen navigation bar
    Signed-off-by: Mark Brindle <mark.brindle@arkemm.com>

--- TicketingHub/Classes/MainScreen/TXHMainViewController.m
+++ TicketingHub/Classes/MainScreen/TXHMainViewController.m
@@ -47,6 +47,40 @@
   [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:1.0f / 255.0f green:46.0f / 255.0f blue:67.0f / 255.0f alpha:1.0f]];
   self.navigationController.navigationBar.titleTextAttributes = @{NSFontAttributeName: [UIFont systemFontOfSize:24.0f],
                                                                   NSForegroundColorAttributeName: [UIColor whiteColor]};
+  
+  NSString *titleString = @"21st Oct 2013";
+  UIFont *font = [UIFont systemFontOfSize:15.0f];
+  NSDictionary *attributesDict = @{NSFontAttributeName : font};
+  NSAttributedString *attributedTitleString = [[NSAttributedString alloc] initWithString:titleString attributes:attributesDict];
+  CGSize titleSize = [attributedTitleString size];
+//  UIImage *buttonBackgroundImage = [[UIImage imageNamed:@"ButtonBackground"] resizableImageWithCapInsets:UIEdgeInsetsMake(1.0, 1.0, 1.0, 1.0)];
+  UIButton *dateBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+  CGRect frame = CGRectZero;
+  frame.size = titleSize;
+  dateBtn.frame = CGRectInset(frame, -5, -5);
+//  [dateBtn setImage:buttonBackgroundImage forState:UIControlStateNormal];
+//  [dateBtn setImage:buttonBackgroundImage forState:UIControlStateSelected];
+  [dateBtn addTarget:self action:@selector(selectDate:) forControlEvents:UIControlEventTouchUpInside];
+  dateBtn.titleLabel.font = font;
+  [dateBtn setTitle:titleString forState:UIControlStateNormal];
+  dateBtn.tintColor = [UIColor whiteColor];
+  
+  UIBarButtonItem *dateButton = [[UIBarButtonItem alloc] initWithCustomView:dateBtn];
+
+  titleString = @"13:00";
+  NSAttributedString *attributedTimeString = [[NSAttributedString alloc] initWithString:titleString attributes:attributesDict];
+  CGSize timeSize = [attributedTimeString size];
+  UIButton *timeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+//  [timeBtn setImage:buttonBackgroundImage forState:UIControlStateNormal];
+//  [timeBtn setImage:buttonBackgroundImage forState:UIControlStateSelected];
+  [timeBtn addTarget:self action:@selector(selectTime:) forControlEvents:UIControlEventTouchUpInside];
+  frame.size = timeSize;
+  timeBtn.frame = CGRectInset(frame, -5, -5);
+  timeBtn.tintColor = [UIColor whiteColor];
+  [timeBtn setTitle:titleString forState:UIControlStateNormal];
+  
+  UIBarButtonItem *timeButton = [[UIBarButtonItem alloc] initWithCustomView:timeBtn];
+  [self.navigationItem setLeftBarButtonItems:@[self.navigationItem.leftBarButtonItem, dateButton, timeButton]];
 }
 
 - (void)dealloc {
@@ -74,6 +108,16 @@
 - (IBAction)toggleMenu:(id)sender {
 #pragma unused (sender)
   [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_TOGGLE_MENU object:nil];
+}
+
+-(void)selectDate:(id)sender {
+#pragma unused (sender)
+  NSLog(@"Date selected");
+}
+
+-(void)selectTime:(id)sender {
+#pragma unused (sender)
+  NSLog(@"Time selected");
 }
 
 #pragma mark - Notifications
