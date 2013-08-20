0269b83f3a12c7f4594614583f0165c7cd18e89b
Abizer Nasir <abizern@abizern.org>
2013-08-20 14:02:07 +0100

    Convert to XCtest from OCUnit

--- TicketingHubTests/TicketingHubTests.h
+++ TicketingHubTests/TicketingHubTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 TicketingHub. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface TicketingHubTests : SenTestCase
+@interface TicketingHubTests : XCTestCase
 
 @end
--- TicketingHubTests/TicketingHubTests.m
+++ TicketingHubTests/TicketingHubTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in TicketingHubTests");
+    XCTFail(@"Unit tests are not implemented yet in TicketingHubTests");
 }
 
 @end
