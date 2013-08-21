938fe5d00c75f3dfd16ea57864f492993d9a2216
Abizer Nasir <abizern@abizern.org>
2013-08-21 23:51:39 +0100

    Documentation by way of comments

--- TicketingHub/Human/TXHOptionMO.h
+++ TicketingHub/Human/TXHOptionMO.h
@@ -4,6 +4,14 @@
 
 @interface TXHOptionMO : _TXHOptionMO {}
 
+/*! Creates the a TXHOptionMO object with the given option
+ *
+ *  This method adds the venue to the venue relationship, so it doesn't need to be set from the other side.
+ *  It is created within the same managed object context as the venue
+ *  \param option a TXHOption object used to create the managed object version.
+ *  \param venue the venue that this option is related to.
+ *  \returns a TXHOptionMO object
+ */
 + (instancetype)createWithOption:(TXHOption *)option forVenue:(TXHVenueMO *)venue;
 
 @end
