15e8492b969737ac24338e59d548865100217afe
Abizer Nasir <abizern@abizern.org>
2013-12-02 12:22:25 +0000

    Documentation for public method in MainViewController

--- TicketingHub/Classes/MainViewController.h
+++ TicketingHub/Classes/MainViewController.h
@@ -11,6 +11,13 @@
 
 @interface MainViewController : UIViewController
 
+/** Show the login view controller
+ 
+ This method makes no changes to the managed object context, so reset it manually if required.
+ 
+ @param animated A boolean that sets whether the view controller is animated.
+ @param completion a block that is run on completion. This block takes no parameters and has no return.s
+ */
 - (void)presentLoginViewControllerAnimated:(BOOL)animated completion:(void(^)(void))completion;
 
 @end
