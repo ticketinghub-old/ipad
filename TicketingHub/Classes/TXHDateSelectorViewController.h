87f0ca8ed62a6169559f1b3d24867858f20fe9c9
Abizer Nasir <abizern@abizern.org>
2014-02-03 16:23:07 +0000

    Update comment for delegate method

--- TicketingHub/Classes/TXHDateSelectorViewController.h
+++ TicketingHub/Classes/TXHDateSelectorViewController.h
@@ -15,6 +15,14 @@ extern NSString * const DateSelectorViewControllerStoryboardIdentifier;
 
 @protocol TXHDateSelectorViewDelegate <NSObject>
 
+@optional
+/** Handle date selection - optional
+ 
+ The delegate is sent this message when a date is selected in the calendar view.
+
+ @param controller The instance of DateSelectorViewController that sent this message.
+ @param date the selected date, the time portion is midnight and should not be relied on.
+ */
 - (void)dateSelectorViewController:(TXHDateSelectorViewController *)controller didSelectDate:(NSDate *)date;
 
 @end
@@ -26,6 +34,11 @@ extern NSString * const DateSelectorViewControllerStoryboardIdentifier;
  A reference to the network library
  */
 @property (strong, nonatomic) TXHTicketingHubClient *ticketingHubClient;
+
+/**
+ The selected date to be highlighted in the calendar. Only the date portion is used.
+ */
+@property (strong, nonatomic) NSDate *selectedDate;
 
 /** 
  A delegate to handle date selection
