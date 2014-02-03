4522c45bec45d0a71386f6c1c690c92a94a23481
Abizer Nasir <abizern@abizern.org>
2014-02-03 15:41:00 +0000

    Add calendar view images
    Image Catalog sliced where necessary.

---
+++ TicketingHub/Classes/TXHCalendarRowCell.h
@@ -0,0 +1,13 @@
+//
+//  TXHCalendarRowCell.h
+//  TicketingHub
+//
+//  Created by Abizer Nasir on 03/02/2014.
+//  Copyright (c) 2014 TicketingHub. All rights reserved.
+//
+
+#import <TimesSquare/TimesSquare.h>
+
+@interface TXHCalendarRowCell : TSQCalendarRowCell
+
+@end
