7f3204f242a007ad818dc21a5537026d78f87cec
Bartek Hugo <bartekhugo@me.com>
2014-03-28 13:26:13 +0100

    Activity view with label

---
+++ TicketingHub/Classes/TXHActivityLabelView.h
@@ -0,0 +1,13 @@
+//
+//  TXHActivityLabelView.h
+//  TicketingHub
+//
+//  Created by Bartek Hugo Trzcinski on 28/03/14.
+//  Copyright (c) 2014 TicketingHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TXHActivityLabelView : UIView
+
+@end
---
+++ TicketingHub/Classes/TXHActivityLabelView.m
@@ -0,0 +1,31 @@
+//
+//  TXHActivityLabelView.m
+//  TicketingHub
+//
+//  Created by Bartek Hugo Trzcinski on 28/03/14.
+//  Copyright (c) 2014 TicketingHub. All rights reserved.
+//
+
+#import "TXHActivityLabelView.h"
+
+@implementation TXHActivityLabelView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
