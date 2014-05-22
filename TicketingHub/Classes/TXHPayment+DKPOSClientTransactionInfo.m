4e3307cd083df5d0c34ea67c7a01372f85d7642d
Bartek Hugo <bartekhugo@me.com>
2014-05-22 14:55:15 +0200

    Proper card schemes

--- TicketingHub/Classes/TXHPayment+DKPOSClientTransactionInfo.m
+++ TicketingHub/Classes/TXHPayment+DKPOSClientTransactionInfo.m
@@ -8,6 +8,7 @@
 
 #import "TXHPayment+DKPOSClientTransactionInfo.h"
 #import "DKPOSClientTransactionInfo.h"
+#import "DKPOSClientTypes.h"
 
 @implementation TXHPayment (DKPOSClientTransactionInfo)
 
@@ -21,12 +22,15 @@
     NSString *deviceSerialNumber = info.userInfo[kDKPOSClientDeviceSerialKey];
     NSString *eFTTransactionID   = info.userInfo[kDKPOSClientTransactionEFTTransactionIDKey];
     
+    NSNumber *issuerID = info.userInfo[kDKPOSClientTransactionIssuerID];
+    NSString *cardScheme = [self cardSchemeFor:(DKCardIssuer)[issuerID integerValue]];
+    
     TXHCard *card = [TXHCard insertInManagedObjectContext:moc];
-    card.scheme = info.userInfo[kDKPOSClientTransactionCardIssuerNameKey];
+    card.scheme = cardScheme;
     
     payment.card               = card;
     payment.type               = @"card";
-    payment.verificationMethod = info.userInfo[kDKPOSClientTransactionCardVerificationMethodKey];
+    payment.verificationMethod = [info.userInfo[kDKPOSClientTransactionCardVerificationMethodKey] lowercaseString];
     payment.inputType          = info.userInfo[kDKPOSClientTransactionCardEntryTypeKey];
     payment.authorization      = info.userInfo[kDKPOSClientTransactionAuthorisationCodeKey];
     payment.reference          = [NSString stringWithFormat:@"%@|%@",deviceSerialNumber, eFTTransactionID];
@@ -34,4 +38,25 @@
     return payment;
 }
 
++ (NSString *)cardSchemeFor:(DKCardIssuer)issuer
+{
+    switch (issuer) {
+        case DKCardIssuerElectron:      return @"visa_electron";
+        case DKCardIssuerMasterCard:    return @"master_card";
+        case DKCardIssuerVisa:          return @"visa";
+        case DKCardIssuerMaestro:       return @"maestro";
+        case DKCardIssuerDinersClub:    return @"diners_club";
+        case DKCardIssuerJCB:           return @"jcb";
+        case DKCardIssuerAMEX:          return @"amex";
+        case DKCardIssuerEurocard:      return @"eurocard";
+        case DKCardIssuerDankort:       return @"dankort";
+        case DKCardIssuerUnionPay:      return @"union_pay";
+        case DKCardIssuerDiscovery:     return @"discover";
+        case DKCardIssuerUndefined:
+        default:
+            return @"unknown";
+    }
+    
+    return nil;
+}
 @end
